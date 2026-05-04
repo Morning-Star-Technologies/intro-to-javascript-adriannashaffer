0b0b113685f8676ee281e80f1e0db7fc2729fd3c
Bryan Nelson <1914742+bryanjnelson@users.noreply.github.com>
2026-05-04 02:07:40 +0000

    Updates conditionals challenge on Adrianna's behalf

--- challenges/control-structures-conditionals/control-structures-conditionals.challenge.js
+++ challenges/control-structures-conditionals/control-structures-conditionals.challenge.js
@@ -6,3 +6,27 @@
 * 2. A switch statement with at least 3 cases and a default case that
 *    logs a message to the console for each case.
 */
+
+let character = 'Garfield';
+let hatesMondays = true;
+let lovesLasagna = true;
+
+if (character === 'Garfield' && lovesLasagna === true) 
+  console.log('Garfield is happily eating lasagna');
+else 
+  console.log("This character is not Garfield or doesn't like lasagna.");
+
+let day = 'Monday';
+
+switch (day) 
+{case 'Monday':
+    console.log('Garfield hates Mondays!');
+    break;
+  case 'Tuesday':
+    console.log('Garfield is still grumpy.');
+    break;
+  case 'Friday':
+    console.log('Garfield is ready to relax!');
+    break;
+  default:
+    console.log('Garfield is just being lazy.')}
